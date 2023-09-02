39cd4b1308f643543962e3c1ad9de605108bebb7
Kitae's Github <aqua0405@gmail.com>
2023-09-02 19:37:33 +0900

    Initial commit

---
+++ src/main/java/kr/inhatc/shop/Shop4Application.java
@@ -0,0 +1,13 @@
+package kr.inhatc.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop4Application.class, args);
+	}
+
+}
