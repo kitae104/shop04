273280506c9a11ba9156f5d3ac2765e0519ef366
Kitae's Github <aqua0405@gmail.com>
2023-09-04 19:48:58 +0900

    Update MemberService.java

--- src/main/java/kr/inhatc/shop/service/MemberService.java
+++ src/main/java/kr/inhatc/shop/service/MemberService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -19,7 +21,10 @@ public class MemberService {
     }
 
     private void validateDuplicateMember(Member member) {
-        Member findMember = memberRepository.findByEmail(member.getEmail())
-                .orElseThrow(()-> new IllegalStateException("이미 가입된 회원입니다."));
+        Optional<Member> findMember = memberRepository.findByEmail(member.getEmail());
+        if(findMember.isPresent()) {
+            System.out.println("member : " + findMember);
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
     }
 }
